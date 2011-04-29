02817c5d7d9df1f814aaff396e77ca1bf63beeb6
mackeni <ian.e.mackenzie@gmail.com>
2011-04-29 13:20:55 -0400

    Update Simplex to use simple vertex storage

--- OpenSolid/Mesh/Simplex.hpp
+++ OpenSolid/Mesh/Simplex.hpp
@@ -21,35 +21,57 @@
 #ifndef OPENSOLID__SIMPLEX_HPP
 #define OPENSOLID__SIMPLEX_HPP
 
-#include "Mesh.hpp"
+#include <OpenSolid/Value/Matrix.hpp>
+#include <OpenSolid/Common/Bounds.hpp>
 
 namespace OpenSolid
 {
+    template <int dimensions_, int size_>
     class Simplex
     {
-    private:
-        Mesh _mesh;
-        int _simplex_index;
     public:
-        Simplex(const Mesh& mesh, int index);
-        
-        const Mesh& mesh() const;
-        int index() const;
-        
-        //const MatrixXd& vertices() const;
+        typedef Matrix<double, dimensions_, size_> VerticesType;
+        typedef typename VerticesType::ConstColXpr VertexType;
+        typedef Matrix<Interval, dimensions_, 1> BoundsType;
+    private:
+        VerticesType _vertices;
+    public:
+        template <class DerivedType>
+        Simplex(const EigenBase<DerivedType>& vertices);
         
         int dimensions() const;
-        int order() const;
+        int size() const;
         
-        const MatrixXd::ColXpr vertex(int index) const;
-        VectorXI bounds() const;
+        const VerticesType& vertices() const;
+        VertexType vertex(int index) const;
+        
+        BoundsType bounds() const;
     };
     
-    template<>
-    struct Bounds<Simplex>
+    typedef Simplex<2, 2> LineSegment2d;
+    typedef Simplex<3, 2> LineSegment3d;
+    typedef Simplex<4, 2> LineSegment4d;
+    typedef Simplex<Dynamic, 2> LineSegmentXd;
+    
+    typedef Simplex<2, 3> Triangle2d;
+    typedef Simplex<3, 3> Triangle3d;
+    typedef Simplex<4, 3> Triangle4d;
+    typedef Simplex<Dynamic, 3> TriangleXd;
+    
+    typedef Simplex<3, 4> Tetrahedron3d;
+    typedef Simplex<4, 4> Tetrahedron4d;
+    typedef Simplex<Dynamic, 4> TetrahedronXd;
+    
+    typedef Simplex<4, 5> Pentachoron4d;
+    typedef Simplex<Dynamic, 5> PentachoronXd;
+    
+    typedef Simplex<Dynamic, Dynamic> SimplexXd;
+    
+    template<int dimensions_, int size_>
+    struct Bounds<Simplex<dimensions_, size_> >
     {
-        typedef VectorXI Type;
-        static VectorXI bounds(const Simplex& simplex);
+        typedef typename Simplex<dimensions_, size_>::BoundsType Type;
+        static Type bounds(const Simplex<dimensions_, size_>& simplex);
     };
 }
 
@@ -57,31 +79,35 @@ namespace OpenSolid
 
 namespace OpenSolid
 {
-    // inline Simplex::Simplex(const Mesh& mesh, int index)
-    // : _mesh(mesh), _index(index) {}
-    // 
-    // //inline const MatrixXd& Simplex::vertices() const {return _data->vertices;}
-    // 
-    // inline const Mesh& Simplex::mesh() const {return _mesh;}
-    // 
-    // inline int Simplex::index() const {return _index;}
-    // 
-    // inline int Simplex::dimensions() const {return mesh().dimensions();}
-    // 
-    // inline int Simplex::order() const {return mesh().order();}
-    // 
-    // inline const MatrixXd::ColXpr Simplex::vertex(int vertex_index) const {
-    //     assert(index >= 0 && vertex_index < size());
-    //     return mesh().vertex(mesh().indices(index)(vertex_index));
-    // }
-    //     
-    // inline VectorXI Simplex::bounds() const {
-    //     VectorXI result = vertex(0).cast<Interval>();
-    //     for (int i = 1; i < size(); ++i) {result = result.hull(vertex(i));}
-    //     return result;
-    // }
-    // 
-    // inline VectorXI Bounds<Simplex>::bounds(const Simplex& simplex) {return simplex.bounds();}
+    template <int dimensions_, int size_> template <class DerivedType>
+    inline Simplex<dimensions_, size_>::Simplex(const EigenBase<DerivedType>& vertices) :
+        _vertices(vertices) {}
+    
+    template <int dimensions_, int size_>
+    inline int Simplex<dimensions_, size_>::dimensions() const {return _vertices.rows();}
+    
+    template <int dimensions_, int size_>
+    inline int Simplex<dimensions_, size_>::size() const {return _vertices.cols();}
+    
+    template <int dimensions_, int size_>
+    inline const typename Simplex<dimensions_, size_>::VerticesType&
+    Simplex<dimensions_, size_>::vertices() const {return _vertices;}
+    
+    template <int dimensions_, int size_>
+    inline typename Simplex<dimensions_, size_>::VertexType
+    Simplex<dimensions_, size_>::vertex(int index) const {return _vertices.col(index);}
+    
+    template <int dimensions_, int size_>
+    inline typename Simplex<dimensions_, size_>::BoundsType
+    Simplex<dimensions_, size_>::bounds() const {
+        return _vertices.rowwise().minCoeff().hull(_vertices.rowwise().maxCoeff());
+    }
+    
+    template <int dimensions_, int size_>
+    inline typename Simplex<dimensions_, size_>::BoundsType
+    Bounds<Simplex<dimensions_, size_> >::bounds(
+        const Simplex<dimensions_, size_>& simplex
+    ) {return simplex.bounds();}
 }
 
 #endif
