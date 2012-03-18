5cafd1c206c6cdf4aa82f2ecbb2d26ee31ff56df
Ian <ian.e.mackenzie@gmail.com>
2012-03-18 15:13:54 -0400

    Change variable name to improve readability a bit

--- macros.cmake
+++ macros.cmake
@@ -12,26 +12,26 @@ macro(create_source_groups)
     list(REMOVE_DUPLICATES PATH_LIST)
     
     # Include subdirectory includes so autocompletion works properly within ALL_SOURCES target
-    foreach(GROUP_PATH ${PATH_LIST})
-        if(EXISTS ${GROUP_PATH}/CMakeLists.txt)
-            get_directory_property(SUBDIR_INCLUDES DIRECTORY "${GROUP_PATH}" INCLUDE_DIRECTORIES)
+    foreach(UNIQUE_PATH ${PATH_LIST})
+        if(EXISTS ${UNIQUE_PATH}/CMakeLists.txt)
+            get_directory_property(SUBDIR_INCLUDES DIRECTORY "${UNIQUE_PATH}" INCLUDE_DIRECTORIES)
             include_directories(${SUBDIR_INCLUDES})
         endif()
     endforeach()
 
     # Set up source groups
     get_filename_component(PARENT_DIRECTORY "${CMAKE_CURRENT_LIST_DIR}/.." ABSOLUTE)
-    foreach(GROUP_PATH ${PATH_LIST})
+    foreach(UNIQUE_PATH ${PATH_LIST})
         # Scan through all source files to find those contained within the current subdirectory
         set(CONTAINED_SOURCES)
         foreach(SOURCE_FILE ${ARGN})
             get_filename_component(SOURCE_PATH ${SOURCE_FILE} PATH)
-            if (${SOURCE_PATH} STREQUAL ${GROUP_PATH})
+            if (${SOURCE_PATH} STREQUAL ${UNIQUE_PATH})
                 list(APPEND CONTAINED_SOURCES ${SOURCE_FILE})
             endif()
         endforeach()
         # Generate group name from subdirectory path
-        string(REPLACE "${PARENT_DIRECTORY}" "" STRIPPED_PATH ${GROUP_PATH})
+        string(REPLACE "${PARENT_DIRECTORY}" "" STRIPPED_PATH ${UNIQUE_PATH})
         string(REGEX REPLACE "/" "\\\\" GROUP_NAME ${STRIPPED_PATH})
         # Create source group
         source_group(${GROUP_NAME} FILES ${CONTAINED_SOURCES})
