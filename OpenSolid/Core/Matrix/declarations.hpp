9ce6e64bc4e0bf10b1720348481b423255170c70
Ian Mackenzie <ian.e.mackenzie@gmail.com>
2012-12-24 10:33:31 -0500

    Add declarations headers to aid with untangling dependencies and reducing compilation times

---
+++ OpenSolid/Core/Matrix/declarations.hpp
@@ -0,0 +1,43 @@
+/***************************************************************************
+ *   Copyright (C) 2007 by Ian Mackenzie                                   *
+ *   ian.e.mackenzie@gmail.com                                             *
+ *                                                                         *
+ *   This program is free software; you can redistribute it and/or modify  *
+ *   it under the terms of the GNU General Public License as published by  *
+ *   the Free Software Foundation; either version 2 of the License, or     *
+ *   (at your option) any later version.                                   *
+ *                                                                         *
+ *   This program is distributed in the hope that it will be useful,       *
+ *   but WITHOUT ANY WARRANTY; without even the implied warranty of        *
+ *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the         *
+ *   GNU General Public License for more details.                          *
+ *                                                                         *
+ *   You should have received a copy of the GNU General Public License     *
+ *   along with this program; if not, write to the                         *
+ *   Free Software Foundation, Inc.,                                       *
+ *   59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.             *
+ ***************************************************************************/
+
+#pragma once
+
+#include <OpenSolid/config.hpp>
+
+namespace opensolid
+{
+	template <class TMatrix>
+    class TransformableMatrix;
+}
+namespace Eigen
+{   
+    namespace internal
+    {
+        template <class TScalar, bool bIsInteger>
+        struct significant_decimals_default_impl;
+        
+        template <class TScalar>
+        struct is_arithmetic;
+    }
+    
+    template <class TScalar>
+    struct NumTraits;
+}
